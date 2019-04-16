54852c5f6e447d985d650b8958be67393e4fdd22
iesmail-znz <49481318+iesmail-znz@users.noreply.github.com>
2019-04-16 16:57:13 +0300

    Changed the tasks code back to delivery task

--- tasks.js
+++ tasks.js
@@ -1,19 +1,19 @@
 [
   {
-    icon: 'family',
-    title: 'task.family_survey.title',
-    appliesTo: 'contacts',
-    appliesToType: [ 'clinic' ],
-    actions: [ { form:'family_survey' } ],
-    events: [ {
-      id: 'family-survey',
-      days:0, start:0, end:14,
-    } ],
-    resolvedIf: function(c, r, event, dueDate) {
-      // Resolved if there a family survey received in time window
-      return isFormFromArraySubmittedInWindow(c.reports, 'family_survey',
-                 Utils.addDate(dueDate, -event.start).getTime(),
-                 Utils.addDate(dueDate,  event.end+1).getTime());
-    },
+    icon: 'mother-child',
+    title: [ { locale:'en', content:'Postnatal visit needed' } ],
+    appliesTo: 'reports',
+    appliesToType: [ 'delivery' ],
+    actions: [ { form:'postnatal_visit' } ],
+    events: [
+      {
+        id: 'postnatal-visit-1',
+        days:7, start:2, end:2,
+      },
+      {
+        id: 'postnatal-visit-2',
+        days:14, start:2, end:2,
+      }
+    ]
   }
 ]
