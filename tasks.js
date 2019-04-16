0c16ad5f0d1d4e3f2786c4e36bde08f0d1096248
iesmail-znz <49481318+iesmail-znz@users.noreply.github.com>
2019-04-16 16:43:32 +0300

    Testing tasks.js

--- tasks.js
+++ tasks.js
@@ -1,1 +1,19 @@
-[]
+[
+  {
+    icon: 'mother-child',
+    title: [ { locale:'en', content:'Postnatal visit needed' } ],
+    appliesTo: 'reports',
+    appliesToType: [ 'delivery' ],
+    actions: [ { form:'postnatal_visit' } ],
+    events: [
+      {
+        id: 'postnatal-visit-1',
+        days:7, start:2, end:2,
+      },
+      {
+        id: 'postnatal-visit-2',
+        days:14, start:2, end:2,
+      }
+    ]
+  }
+]
