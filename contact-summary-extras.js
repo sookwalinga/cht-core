c80827e39c8b36959414c39e4a81350cc2368399
Heiko Hornung <hhornung@d-tree.org>
2019-04-25 15:56:46 +0300

    fixing baseline task bugs (open issues: task triggered for CHV person, task doesn't disapepar yet)

--- contact-summary-extras.js
+++ contact-summary-extras.js
@@ -1,8 +1,11 @@
 function isChildUnder5() {
-  var birthDate = new Date(contact.date_of_birth);
-  var ageInMs = new Date(now - birthDate.getTime());
-  var ageInMonths = (Math.abs(ageInMs.getFullYear() - 1970) * 12) + ageInMs.getMonth();
-  return ageInMonths < 60;
+  if(contact && contact.date_of_birth) {
+    var birthDate = new Date(contact.date_of_birth);
+    var ageInMs = new Date(now - birthDate.getTime());
+    var ageInMonths = (Math.abs(ageInMs.getFullYear() - 1970) * 12) + ageInMs.getMonth();
+    return ageInMonths < 60;
+  }
+  return false;
 }
 
 function countReportsSubmitted(form) {
@@ -14,31 +17,27 @@ function countReportsSubmitted(form) {
 }
 
 function hasGivenConsent() {
+  var consent = '';
   var reportsFound = [];
   reportsFound = contact.reports.filter(function(r) {
     return r.form === 'infant_child' && r.fields && r.fields.consent && r.fields.consent.child_consent_today !== '';
   });
   if(reportsFound.length > 0){
-    reportsFound.forEach(function(r) {
-      if(r.consent.child_consent_today === 'yes') {
-        return 'yes';
-      }
-    });
+    var report = getMostRecentReport(reportsFound, 'infant_child');
+    consent = report.fields.consent.child_consent_today;
   }
-  return 'no';
+  return consent;
 }
 
 function isSmallBaby() {
+  var small ="";
   var reportsFound = [];
   reportsFound = contact.reports.filter(function(r) {
     return r.form === 'infant_child' && r.fields && r.fields.first_visit_6_months && r.fields.first_visit_6_months.small_baby_today !== '';
   });
   if(reportsFound.length > 0){
-    reportsFound.forEach(function(r) {
-      if(r.first_visit_6_months.small_baby_today === 'yes') {
-        return 'yes';
-      }
-    });
+    var report = getMostRecentReport(reportsFound, 'infant_child');
+    small = report.fields.first_visit_6_months.small_baby_today;
   }
-  return 'no';
+  return small;
 }
--- tasks.js
+++ tasks.js
@@ -1,16 +1,18 @@
-module.exports = [
+[
   // first infant-child visit
+  // FIXME add translation key 'task.infant_child'
   {
     icon: 'child',
-    title: 'task.infant_child',
+    title: [ {locale: 'en', content: 'Infant-Child visit'} ],
     appliesTo: 'contacts',
-    appliesIf: function(c, r) {
-      return isChildUnderFive(c);
+    appliesIf: function(c) {
+      console.log("Logging from tasks ...", c);
+      return isChildUnder5(c);
     },
     appliesToType: [ 'person' ],
     actions: [{
       form: 'infant_child',
-      modifyContent: function(content, c, r) {
+      modifyContent: function(c, content) {
         content.child_consent = hasGivenConsent(c);
         content.num_child_visits = countReportsSubmitted(c, 'infant_child');
         content.small_baby = isSmallBaby(c);
@@ -18,8 +20,14 @@ module.exports = [
     }],
     events: [
       {
-        id:'infant_child_first_visit', days:0, start:0, end:300,
+        id:'infant_child_first_visit', days:0, start:0, end:21,
       }
-    ]
+    ],
+    resolvedIf: function(c, r, event, dueDate) {
+      // Resolved if there is a form submitted within the time window
+      return isFormSubmittedInWindow(c.reports, 'infant_child',
+                 Utils.addDate(dueDate, -event.start).getTime(),
+                 Utils.addDate(dueDate,  event.end+1).getTime());
+    },
   }
-];
+]
