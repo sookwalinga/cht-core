a0617742a2581cfab6ea9b2026c12dd9df1bd1aa
hhornung <hhornung@d-tree.org>
2019-05-02 17:15:09 +0300

    add referral reason messages

--- nools-extras.js
+++ nools-extras.js
@@ -95,7 +95,27 @@ function referralMade(report) {
 }
 
 function getReferralReasons(report) {
-  // if this is to work for all forms, we'll need a standard way to store this
-  console.log("Satisfying jshint...", report);
-  return "some very good reason<br>and another one";
+  var reasons = '';
+  if(report.fields.first_visit_6_months.refer_flag_small_baby === '1') {
+    reasons += 'referral.infant_child.refer_flag_small_baby <br> ';
+  }
+  if(report.fields.neonatal_danger_signs.refer_neonatal_danger_sign_flag === '1') {
+    reasons += 'referral.infant_child.refer_neonatal_danger_sign_flag <br> ';
+  }
+  if(report.fields.child_danger_signs.refer_child_danger_sign_flag === '1') {
+    reasons += 'referral.infant_child.refer_child_danger_sign_flag <br> ';
+  }
+  if(report.fields.malnutrition_anemia.refer_muac_flag === '1') {
+    reasons += 'referral.infant_child.refer_muac_flag <br> ';
+  }
+  if(report.fields.malnutrition_anemia.refer_palm_pallor_flag === '1') {
+    reasons += 'referral.infant_child.refer_palm_pallor_flag <br> ';
+  }
+  if(report.fields.immunizations.refer_vaccines_flag === '1') {
+    reasons += 'referral.infant_child.refer_vaccines_flag <br> ';
+  }
+  if(report.fields.problem_solving.refer_slow_to_lear_specifics_flag === '1') {
+    reasons += 'referral.infant_child.refer_slow_to_lear_specifics_flag <br> ';
+  }
+  return reasons;
 }
