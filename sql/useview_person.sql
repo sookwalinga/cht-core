02bb3af1f71ad48f53263efc3c92b2ade7e98fac
Sam Lilienfeld <46962676+slilienfeld-DTree@users.noreply.github.com>
2019-07-19 17:16:45 +0300

    Postgres materialized views (#42)
    Correcting view name inconsistencies and bringing in to line with Medic naming standards.
    Adding additional materialized views for CHVs, Supervisors, catchment areas and supervisory areas.

--- sql/useview_person.sql
+++ sql/useview_person.sql
@@ -1,6 +1,8 @@
 ------------------------------------------------------------
 -- Materialized view to show table of all people.
 ------------------------------------------------------------
+DROP MATERIALIZED VIEW IF EXISTS useview_person;
+
 CREATE MATERIALIZED VIEW useview_person AS 
 (
   SELECT
@@ -16,21 +18,21 @@ CREATE MATERIALIZED VIEW useview_person AS
     doc ->> 'phone' AS phone,
     doc ->> 'alternate_phone' AS alternate_phone,
     CASE WHEN doc ->> 'is_head_of_household' = 'yes' THEN 'yes' ELSE 'no' END AS is_head_of_household,
-    doc#>>'{meta,created_by}' AS created_by,
-    doc#>>'{meta,created_by_place_uuid}' AS created_by_place_uuid,
-    doc#>>'{meta,created_by_person_uuid}' AS chv_uuid,
+    doc #>> '{meta,created_by}' AS created_by,
+    doc #>> '{meta,created_by_place_uuid}' AS created_by_place_uuid,
+    doc #>> '{meta,created_by_person_uuid}' AS chv_uuid,
     to_timestamp((NULLIF(doc ->> 'reported_date', '')::bigint / 1000)::double precision) AS reported_date,
     doc ->> 'type' AS type,
-    doc#>>'{parent,_id}' AS household_uuid,
-    doc#>>'{parent,parent,_id}' AS catchment_area_uuid,
-    doc#>>'{parent,parent,parent,_id}' AS supervisory_area_uuid        
+    doc #>> '{parent,_id}' AS household_uuid,
+    doc #>> '{parent,parent,_id}' AS catchment_area_uuid,
+    doc #>> '{parent,parent,parent,_id}' AS supervisory_area_uuid        
   FROM 
 	  couchdb	
   WHERE 
-	  doc->>'type' = 'person' AND doc#>>'{parent,parent,parent,_id}' IS NOT NULL
+	  doc ->> 'type' = 'person' AND doc #>> '{parent,parent,parent,_id}' IS NOT NULL
 );
 
-CREATE UNIQUE INDEX IF NOT EXISTS person_reported_date_created_by_uuid ON person USING btree (reported_date, chv_uuid, _id);
+CREATE UNIQUE INDEX IF NOT EXISTS person_reported_date_created_by_uuid ON useview_person USING btree (reported_date, chv_uuid, _id);
 -- Permissions
 REASSIGN OWNED BY current_user TO full_access;
-GRANT SELECT ON household TO full_access, dtree, periscope;
+GRANT SELECT ON useview_person TO full_access, dtree, periscope;
