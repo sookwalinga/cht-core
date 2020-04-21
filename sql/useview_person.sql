d349ce7a35deb55317cde55c8351aa93c57bc72d
Sam Lilienfeld <46962676+slilienfeld-DTree@users.noreply.github.com>
2020-04-21 10:29:50 -0400

    Adding missing fields to useview_person (#133)
    * Adding node_modules to .gitignore and adding missing fields to useview_person.sql
    
    * Adjusted view definition to flexibly capture created_by and chv_uuid fields from either standard person doc format or person document created by updated pregnancy_outcomes form
    
    * Reformatting query per style guide

--- sql/useview_person.sql
+++ sql/useview_person.sql
@@ -6,25 +6,33 @@ DROP MATERIALIZED VIEW IF EXISTS useview_person;
 CREATE MATERIALIZED VIEW useview_person AS 
 (
   SELECT
-    doc ->> '_id' AS _id,
-    doc ->> '_rev' AS _rev,
-    doc ->> 'first_name' AS first_name,
-    doc ->> 'middle_name' AS middle_name,
-    doc ->> 'last_name' AS last_name,
-    doc ->> 'name' AS name,
-    doc ->> 'sex' AS sex,
-    NULLIF(doc ->> 'exact_dob_known', '')::BOOLEAN AS exact_dob_known,
-    TO_DATE(doc ->> 'date_of_birth', 'YYYY-MM-DD') AS date_of_birth,
-    doc ->> 'phone' AS phone,
-    doc ->> 'alternate_phone' AS alternate_phone,
-    (CASE WHEN doc ->> 'is_head_of_household' = 'yes' THEN 'yes' ELSE 'no' END)::BOOLEAN AS is_head_of_household,
-    doc #>> '{meta,created_by}' AS created_by,
-    doc #>> '{meta,created_by_person_uuid}' AS chv_uuid,
-    TO_TIMESTAMP((NULLIF(doc ->> 'reported_date', '')::BIGINT / 1000)::DOUBLE PRECISION) AS reported_date,
-    doc ->> 'type' AS type,
-    doc #>> '{parent,_id}' AS household_uuid,
-    doc #>> '{parent,parent,_id}' AS catchment_area_uuid,
-    doc #>> '{parent,parent,parent,_id}' AS supervisory_area_uuid        
+    doc ->> '_id' AS _id
+    , doc ->> '_rev' AS _rev
+    , doc ->> 'first_name' AS first_name
+    , doc ->> 'middle_name' AS middle_name
+    , doc ->> 'last_name' AS last_name
+    , doc ->> 'name' AS name
+    , doc ->> 'sex' AS sex
+    , NULLIF(doc ->> 'exact_dob_known', '')::BOOLEAN AS exact_dob_known
+    , TO_DATE(doc ->> 'date_of_birth', 'YYYY-MM-DD') AS date_of_birth
+    , doc ->> 'phone' AS phone
+    , doc ->> 'alternate_phone' AS alternate_phone
+    , (CASE WHEN doc ->> 'is_head_of_household' = 'yes' THEN 'yes' ELSE 'no' END)::BOOLEAN AS is_head_of_household
+    , doc ->> 'temp_hh_member' AS temp_hh_member
+    , doc ->> 'muted' AS muted
+    , CASE  
+        WHEN doc #>> '{meta,created_by}' IS NOT NULL THEN doc #>> '{meta,created_by}'
+        WHEN doc #>> '{meta,created_by}' IS NULL THEN doc ->> 'created_by'
+      END AS created_by
+    , CASE
+        WHEN doc #>> '{meta,created_by_person_uuid}' IS NOT NULL THEN doc #>> '{meta,created_by_person_uuid}'
+        WHEN doc #>> '{meta,created_by_person_uuid}' IS NULL THEN doc ->> 'created_by_person_uuid'
+      END AS chv_uuid
+    , TO_TIMESTAMP((NULLIF(doc ->> 'reported_date', '')::BIGINT / 1000)::DOUBLE PRECISION) AS reported_date
+    , doc ->> 'type' AS type
+    , doc #>> '{parent,_id}' AS household_uuid
+    , doc #>> '{parent,parent,_id}' AS catchment_area_uuid
+    , doc #>> '{parent,parent,parent,_id}' AS supervisory_area_uuid        
   FROM 
 	  couchdb	
   WHERE 
