5c84da7a030f23eae5c9094b71cfa81c42f271aa
nKataraia <55187908+nKataraia@users.noreply.github.com>
2021-09-03 16:08:15 +0300

    Added left out shehia due name collisions (#81) (#85)
    * Added shehias which were forgotten due to collision in names with shehia in other Districts
    and removed duplicated entries

--- sql/useview_jna_locations.sql
+++ sql/useview_jna_locations.sql
@@ -22,21 +22,41 @@ CREATE MATERIALIZED VIEW useview_jna_locations AS
   catchment_CTE AS (
     SELECT 
       doc->>'_id' AS _id,
-      doc->>'shehia' AS shehia 
+      doc->>'shehia' AS shehia,
+      doc->'parent'->>'_id' AS supervisory_area_uuid 
     FROM couchdb 
     WHERE doc->>'type'='health_center'
       AND doc->>'name'!='DEV_CATCHMENT_AREA'
+  ),
+  facility_CTE AS (
+    SELECT 
+      doc->>'_id' AS _id,
+      doc->>'district' AS district,
+      doc->>'name' AS facility 
+    FROM couchdb 
+    WHERE doc->>'type'='district_hospital'
+      AND doc->>'name'!='DEV_SUPERVISORY_AREA'
   )
         
    SELECT DISTINCT
      catchment_CTE._id AS catchment_area_uuid,
      shehia_CTE.shehia,
+     facility_CTE.facility,
      shehia_CTE.ward,
      shehia_CTE.district,
      shehia_CTE.region
    FROM catchment_CTE
+   INNER JOIN facility_CTE
+   ON facility_CTE._id=catchment_CTE.supervisory_area_uuid
+   -- Left joining to be able to see the missing shehia names for the rare cases when new catchment are created and shehia name get misspelled 
    LEFT JOIN shehia_CTE
      ON shehia_CTE.shehia=catchment_CTE.shehia
+     AND (
+       shehia_CTE.district=facility_CTE.district
+       -- Catchments in Dodo and Pete are in different district from where there supervisory_areas are located
+       --also these names are not duplicated which allows us to include them here. 
+        OR  catchment_CTE.shehia in ('Dodo','Pete')
+     )
 );
 CREATE UNIQUE INDEX IF NOT EXISTS jna_location_catchment_uuid ON useview_jna_locations USING btree (catchment_area_uuid);
 -- Permissions
