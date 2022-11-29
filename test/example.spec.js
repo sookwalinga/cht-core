1f095a91e696197ab735d181a1ae41a259c29981
nKataraia <55187908+nKataraia@users.noreply.github.com>
2022-11-29 20:52:08 +0300

    added new ECD Content in matview  (#150)

--- test/example.spec.js
+++ test/example.spec.js
@@ -1,14 +1,6 @@
 const { expect } = require('chai');
-const TestHarness = require('medic-conf-test-harness');
-const harness = new TestHarness();
-
-describe('example test suite', () => {
-  before(async () => { return await harness.start(); });
-  after(async () => { return await harness.stop(); });
-  beforeEach(async () => { return await harness.clear(); });
-  afterEach(() => { expect(harness.consoleErrors).to.be.empty; });
-
-  it('example test', async () => {
-    expect(!!'true').to.be.true;
-  });
+describe('example test suite', () => { 
+  it('example test',function() {
+    expect(true).to.eq(true);
+  }); 
 });
